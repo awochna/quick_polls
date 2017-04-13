35fe460e0846535413453420fdee9673138b6f42
Alex Wynter <awochna@awochna.com>
2017-04-13 00:49:07 -0700

    Clean up index page

--- test/web/controllers/page_controller_test.exs
+++ test/web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule QuickPolls.Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Make fun polls"
   end
 end
